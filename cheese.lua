aa9ad384941c31de0bf225289ef2789ba8d3c59b
Beha <shacknetisp@mail.com>
2018-10-21 14:55:08 -0400

    Cheese improvements.

--- cheese.lua
+++ cheese.lua
@@ -5,20 +5,10 @@ minetest.register_biome({
     node_stone = "mobs:cheeseblock",
     node_water_top = "mobs:cheeseblock",
     depth_water_top = 10,
-    y_max = weird_biomes.max.y,
-    y_min = -8,
+    y_max = 4,
+    y_min = -48,
     heat_point = 20,
     humidity_point = 65,
 })
 
-minetest.register_biome({
-    name = "cheese_ocean",
-    node_top = "mobs:cheeseblock",
-    depth_top = 1,
-    node_water_top = "mobs:cheeseblock",
-    depth_water_top = 10,
-    y_max = -9,
-    y_min = weird_biomes.secondary_min,
-    heat_point = 0,
-    humidity_point = 65,
-})
+weird_biomes.include("cheese_decorations.lua")
